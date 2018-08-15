718246d8ce0e835fba980351082b6d610b0c9ae0
stormshadow9999 <39254609+stormshadow9999@users.noreply.github.com>
2018-08-15 16:57:58 +0530

    test

--- index.php
+++ index.php
@@ -52,7 +52,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
   </div>
 </section>
 
-
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script type="text/javascript" src="js/popper.min.js"></script>
